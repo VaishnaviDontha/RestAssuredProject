029f5475945a2d80acc5df6e56659ed1c07f7bdd
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-07-18 17:49:51 -0400

    POJO classes

---
+++ bddstyle/src/test/java/POJOClasses/category.java
@@ -0,0 +1,28 @@
+package POJOClasses;
+
+public class category {
+
+    private String categoryID;
+    private String categoryName;
+
+    public void category() {
+
+    }
+
+    public String getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(String categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+}
