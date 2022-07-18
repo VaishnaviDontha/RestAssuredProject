e19797f979b1a1c61c6a5930780021bc07bf1dc3
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-07-18 17:49:58 -0400

    POJO classes

---
+++ bddstyle/src/test/java/POJOClasses/collectionIsbn.java
@@ -0,0 +1,21 @@
+package POJOClasses;
+
+public class collectionIsbn {
+
+    private String isbn;
+
+    public void collectionIsbn() {
+        
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    
+
+}
