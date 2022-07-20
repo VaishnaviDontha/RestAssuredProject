205fbdac922750d7ebc2b56568e9114097d810c7
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-07-20 17:05:44 -0400

    RestAssure BDD Style

--- bddstyle/src/test/java/com/usingAuthentication/basicAuth.java
+++ bddstyle/src/test/java/com/usingAuthentication/basicAuth.java
@@ -15,6 +15,8 @@ import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
+import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.*;
 
 public class basicAuth {
 
@@ -23,22 +25,21 @@ public class basicAuth {
     ValidatableResponse validatableResponse;
 
     @Test
-    public void authToken() throws JsonProcessingException {
+    public void basicAuthentication() throws JsonProcessingException {
 
         RestAssured.baseURI = "https://bookstore.toolsqa.com";
-		
-		RequestSpecification request = RestAssured.given();
-		
-		
-		String credentials = "TOOLSQA-Test:Test@@123";
-		
-		byte[] encodedCredentials = Base64.encodeBase64(credentials.getBytes());
-		
-		String encodedCredentialsAsString = new String(encodedCredentials);
-		
-		request.header("Authorization","Basic "+encodedCredentialsAsString);
-		
-		collectionIsbn isbn = new collectionIsbn();
+
+        RequestSpecification request = RestAssured.given();
+
+        String credentials = "TOOLSQA-Test:Test@@123";
+
+        byte[] encodedCredentials = Base64.encodeBase64(credentials.getBytes());
+
+        String encodedCredentialsAsString = new String(encodedCredentials);
+
+        request.header("Authorization", "Basic " + encodedCredentialsAsString);
+
+        collectionIsbn isbn = new collectionIsbn();
         isbn.setIsbn("9781593275846");
 
         List<collectionIsbn> isbnList = new ArrayList<collectionIsbn>();
@@ -47,14 +48,41 @@ public class basicAuth {
         payloadPOJO payload = new payloadPOJO();
         payload.setUserId("0fd523c4-e61e-4b6a-8abb-64a56d94876f");
         payload.setCollectionOfIsbns(isbnList);
-		
-		request.header("Content-Type","application/json");
-		
-		Response response = request.body(payload).post("/BookStore/v1/Books");
-		
-		System.out.println("Response Status Code is "+response.getStatusCode());
-		
-		response.prettyPrint();
+
+        request.header("Content-Type", "application/json");
+
+        Response response = request.body(payload).post("/BookStore/v1/Books");
+
+        System.out.println("Response Status Code is " + response.getStatusCode());
+
+        response.prettyPrint();
     }
-    
+
+    @Test
+    public void basicAuthenticationBDDStyle() {
+
+        String credentials = "TOOLSQA-Test:Test@@123";
+		byte[] encodedCredentials = Base64.encodeBase64(credentials.getBytes());
+		String encodedCredentialsAsString = new String(encodedCredentials);
+
+
+        collectionIsbn isbn = new collectionIsbn();
+        isbn.setIsbn("9781593275846");
+
+        List<collectionIsbn> isbnList = new ArrayList<collectionIsbn>();
+        isbnList.add(isbn);
+
+        payloadPOJO payload = new payloadPOJO();
+        payload.setUserId("0fd523c4-e61e-4b6a-8abb-64a56d94876f");
+        payload.setCollectionOfIsbns(isbnList);
+
+        given().baseUri("https://bookstore.toolsqa.com")
+                .header("Authorization", "Basic "+encodedCredentialsAsString, "Content-type", "application/json")
+                .body(payload).
+        when().post("/BookStore/v1/Books").
+        then().assertThat().statusCode(200).and().body(" ", equalTo(" "))
+              .and().header("headerName", "expectedValueMatcher");
+        
+    }
+
 }
