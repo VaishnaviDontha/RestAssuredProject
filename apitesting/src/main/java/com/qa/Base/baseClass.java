2f2713e9b8b594394a76e941493bbd7740527a09
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-07-22 15:14:15 -0400

    Base class

--- apitesting/src/main/java/com/qa/Base/baseClass.java
+++ apitesting/src/main/java/com/qa/Base/baseClass.java
@@ -1,5 +1,41 @@
 package com.qa.Base;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Properties;
+
 public class baseClass {
-    
+
+    public int Response_Status_Code_200 = 200;
+   public int Response_Status_Code_201 = 201;
+   public int Response_Status_Code_400 = 400;
+   public int Response_Status_Code_401 = 401;
+   public int Response_Status_Code_500 = 500;
+
+   public static Properties prop;
+
+   public baseClass() {
+
+      try (
+         
+         InputStream input = new FileInputStream("src/main/java/com/qa/Config/dev.properties")) {
+
+         prop = new Properties();
+
+         // load a properties file
+         prop.load(input);
+
+         // get the property value and print it out
+         // System.out.println(prop.getProperty("url"));
+         // System.out.println(prop.getProperty("serviceurl"));
+
+      } catch (IOException ex) {
+         ex.printStackTrace();
+      }
+
+   }
+
+   
 }
