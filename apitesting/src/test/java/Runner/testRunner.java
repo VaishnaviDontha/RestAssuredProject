fdf4da96a399355ad696ba9f76302643feba3bf9
VaishnaviDontha <vaishnavidontha24@gmail.com>
2022-07-25 12:31:32 -0400

    Added reporting details

--- apitesting/src/test/java/Runner/testRunner.java
+++ apitesting/src/test/java/Runner/testRunner.java
@@ -5,7 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
     features = "src/test/Resources/Features",
-    glue = "stepdefs"
+    glue = "stepdefs",
+        plugin = {"pretty", "html:target/cucumber-reports"},
+        monochrome = true
 )
 public class testRunner extends AbstractTestNGCucumberTests{
 
